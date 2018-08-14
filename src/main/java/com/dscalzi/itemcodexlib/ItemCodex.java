c3ca88740290bab38c398377cdc3100cab78ffe9
Daniel Scalzi <d_scalzi@yahoo.com>
2018-08-14 19:13:07 -0400

    v1.0.0-pre.4

--- src/main/java/com/dscalzi/itemcodexlib/ItemCodex.java
+++ src/main/java/com/dscalzi/itemcodexlib/ItemCodex.java
@@ -38,7 +38,7 @@ import com.google.gson.stream.JsonReader;
  */
 public class ItemCodex {
 
-    public static final String VERSION = "1.0.0-pre.3";
+    public static final String VERSION = "1.0.0-pre.4";
     public static final String CODEX_FILE_NAME = "items.json";
     
     private ItemList loadedList;
